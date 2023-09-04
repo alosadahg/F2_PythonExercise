0fc618dde43653bd91e2a72bb002ec538809caa9
hg <hgalosada@gmail.com>
2023-09-04 15:09:25 +0800

    added answer for problem 3

--- 3_numDigits.py
+++ 3_numDigits.py
@@ -1,2 +1,18 @@
-n = input("Please enter a number: ")
-nLen = len(n)
+scan = input("Please enter a number: ")
+numDigits = 0;
+maxN = 0
+minN = 0
+for c in scan:
+    if c.isdigit():
+        c = int(c)
+        if(numDigits == 0):
+            minN = c
+        numDigits += 1
+        if(c < minN):
+            minN = c
+        if(c > maxN):
+            maxN = c
+print("Number of digits in the given number is: ", numDigits)
+print("Smallest number is: ", minN)
+print("Highest number is: ", maxN)
+
